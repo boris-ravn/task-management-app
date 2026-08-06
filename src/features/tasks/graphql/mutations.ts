b6680d2858a0c9b7b75c2d5bb0d7e909978fe5ee
boris-ravn <borismartinez@ravn.co>
2026-08-06 16:50:22 -0400

    feat: add update and delete task mutations and hooks

--- src/features/tasks/graphql/mutations.ts
+++ src/features/tasks/graphql/mutations.ts
@@ -17,3 +17,29 @@ export const CREATE_TASK = gql`
     }
   }
 `;
+
+export const UPDATE_TASK = gql`
+  mutation UpdateTask($input: UpdateTaskInput!) {
+    updateTask(input: $input) {
+      id
+      name
+      status
+      pointEstimate
+      dueDate
+      tags
+      assignee {
+        id
+        fullName
+        avatar
+      }
+    }
+  }
+`;
+
+export const DELETE_TASK = gql`
+  mutation DeleteTask($input: DeleteTaskInput!) {
+    deleteTask(input: $input) {
+      id
+    }
+  }
+`;
